62a475ddd50447b11e651fa4f67e1696b79f3b58
Artis Brizs <artis.brizs@student.griffith.ie>
2022-01-05 23:34:48 +0000

    Created project

---
+++ src/main/java/com/magnet/web_photos/webphotos/WebphotosApplication.java
@@ -0,0 +1,13 @@
+package com.magnet.web_photos.webphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebphotosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebphotosApplication.class, args);
+    }
+
+}
