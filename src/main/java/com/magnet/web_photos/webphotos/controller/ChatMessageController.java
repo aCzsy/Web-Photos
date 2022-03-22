17ff36aeebafb0e4106835a5d9d817c2aac56b10
Artis Brizs <artis.brizs@student.griffith.ie>
2022-03-22 23:30:06 +0000

    working on messaging

---
+++ src/main/java/com/magnet/web_photos/webphotos/controller/ChatMessageController.java
@@ -0,0 +1,17 @@
+package com.magnet.web_photos.webphotos.controller;
+
+import com.magnet.web_photos.webphotos.dto.ChatMessageDTO;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+    // Handles messages from /app/chat. (Note the Spring adds the /app prefix for us).
+    @MessageMapping("/chat")
+    // Sends the return value of this method to /topic/messages
+    @SendTo("/topic/messages")
+    public ChatMessageDTO getMessages(ChatMessageDTO chatMessageDTO){
+        return chatMessageDTO;
+    }
+}
---
+++ src/main/java/com/magnet/web_photos/webphotos/dto/ChatMessageDTO.java
@@ -0,0 +1,13 @@
+package com.magnet.web_photos.webphotos.dto;
+
+public class ChatMessageDTO {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
