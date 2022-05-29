71e2845d1664a232983a0cbe92c9adb4d62c4179
Artis Brizs <artis.brizs@student.griffith.ie>
2022-05-29 19:10:53 +0100

    small fixes

--- src/main/java/com/magnet/web_photos/webphotos/config/DataSourceConfig.java
+++ src/main/java/com/magnet/web_photos/webphotos/config/DataSourceConfig.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
-public class DataSourceConfig {
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://us-cdbr-east-05.cleardb.net/heroku_20faf1f6297f587?password=pf9dc9b01&reconnect=true&user=b71e234c955723");
-        dataSourceBuilder.username("b71e234c955723");
-        dataSourceBuilder.password("pf9dc9b01");
-        return dataSourceBuilder.build();
-    }
-}
+//@Configuration
+//public class DataSourceConfig {
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSourceBuilder.url("jdbc:mysql://us-cdbr-east-05.cleardb.net/heroku_20faf1f6297f587?password=pf9dc9b01&reconnect=true&user=b71e234c955723");
+//        dataSourceBuilder.username("b71e234c955723");
+//        dataSourceBuilder.password("pf9dc9b01");
+//        return dataSourceBuilder.build();
+//    }
+//}
